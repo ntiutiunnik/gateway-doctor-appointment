341bb2eb7901c0831fe61ba9f0dcbc5c3f366b28
ntiutiunnik <puffbeatsale@gmail.com>
2023-02-22 15:44:27 +0400

    Initial setup introduced.

---
+++ src/test/java/com/demo/gateway/doctorappointment/GatewayDoctorAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.gateway.doctorappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayDoctorAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
